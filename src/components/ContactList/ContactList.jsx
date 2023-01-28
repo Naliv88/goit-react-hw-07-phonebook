1ef81088940c184e73555273e35d22c9c6bd5686
Nalyvaiko Pavel <104713787+Naliv88@users.noreply.github.com>
2023-01-28 11:46:57 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import style from './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { delet } from 'redux/slice';
+
+export const ContactList = () => {
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const filteredContacts = () =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+  const contactsList = filter ? filteredContacts() : contacts;
+
+  return (
+    <ul className={style.list}>
+      {contactsList.map(contact => {
+        return (
+          <li className={style.listItem} key={contact.id}>
+            <p>
+              {contact.name}: {contact.number}
+            </p>
+            <button
+              className={style.btnDelete}
+              onClick={() => dispatch(delet(contact.id))}
+              type="button"
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
